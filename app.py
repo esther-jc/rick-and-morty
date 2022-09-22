e32d6694b01afb2d4628badb463aa0fdda739020
echoe2 <110744958+esther-jc@users.noreply.github.com>
2022-09-22 17:10:09 -0400

    created navbar and general template structure

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
-string = "hello"
+from flask import Flask, render_template, url_for, request, redirect, flash, session
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html',
+                            title="Rickords")
+
+@app.route('/records/')
+def viewRecords():
+    return "insert records here"
+
+@app.route('/favorites/')
+def viewFavs():
+    return render_template('favorites.html',
+                            title="Favorites")
+
+@app.route('/map/')
+def viewMap():
+    return "insert map here"
+
+@app.route('/')
+def login():
+    return "insert login pop-up modal here"
+
+if __name__ == "__main__":
+    app.run(debug=True)
